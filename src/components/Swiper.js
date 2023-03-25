d301312ef2c3c9e0ec20c69ec54a16239a5db3f2
abdullo69 <abdulloakbarov28@gmail.com>
2023-03-25 13:52:14 +0500

    swipers

---
+++ src/components/Swiper.js
@@ -0,0 +1,65 @@
+// import Swiper core and required modules
+import { Navigation, Pagination, Scrollbar, A11y, autoplay, Autoplay } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+import 'swiper/css/autoplay'
+import "./Swiper.scss"
+import { AiOutlineRight, AiOutlineLeft } from "react-icons/ai"
+
+import swiper1 from "../Images/swiper1.jpg"
+import swiper2 from "../Images/swiper2.jpg"
+import swiper3 from "../Images/swiper3.jpg"
+import swiper4 from "../Images/swiper4.jpg"
+import swiper5 from "../Images/swiper5.jpg"
+import swiper6 from "../Images/swiper6.jpg"
+import swiper7 from "../Images/swiper7.jpg"
+import swiper8 from "../Images/swiper8.jpg"
+
+export default () => {
+    return (
+        <div>
+            <Swiper className='swiper'
+                // install Swiper modules
+                modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay]}
+                spaceBetween={50}
+                slidesPerView={1}
+                autoplay={{ delay: 1000 }}
+                navigation={{
+                    prevEl: ".left",
+                    nextEl: ".right",
+                }}
+                pagination
+                loop={true}
+
+
+                onSwiper={(swiper) => console.log(swiper)}
+                onSlideChange={() => console.log('slide change')}
+            >
+                <SwiperSlide><img src={swiper4} alt="" /></SwiperSlide>
+                <SwiperSlide><img src={swiper2} alt="" /></SwiperSlide>
+                <SwiperSlide><img src={swiper3} alt="" /></SwiperSlide>
+                <SwiperSlide><img src={swiper1} alt="" /></SwiperSlide>
+                <SwiperSlide><img src={swiper5} alt="" /></SwiperSlide>
+                <SwiperSlide><img src={swiper7} alt="" /></SwiperSlide>
+                <SwiperSlide><img src={swiper8} alt="" /></SwiperSlide>
+
+                <div className='left'>
+                    <AiOutlineLeft />
+                </div>
+                <div className='right'>
+                    <AiOutlineRight />
+                </div>
+                <div className="swiper-pagination"></div>
+            </Swiper>
+
+
+        </div>
+
+    );
+};
