d1bf173675637af2020d8d51fef9e4e32c1aaa06
abdullo69 <abdulloakbarov28@gmail.com>
2023-03-28 14:50:17 +0500

    dawdwwdaw

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.scss';
+import Swiper from './components/swiper/Swiper';
 import Navbar from './components/navbar/Navbar';
-import Footer from "./components/footer/Footer"
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Footer />
+
+      <Swiper />
     </div>
   );
 }
