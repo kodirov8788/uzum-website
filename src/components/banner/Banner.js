d37575a4add11f8185ca92d24887a6d9df10289b
Omonulla Usmonjonov <usmonojnovomonulla@gmail.com>
2023-03-28 15:18:29 +0500

    sdsd

---
+++ src/components/banner/Banner.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "../banner/Banner.css"
+function Banner() {
+    return (
+        <div className='banner'>
+            <img src="https://images.uzum.uz/cfublbng49devoa9a0fg/main_page_banner.jpg" alt="" />
+        </div>
+    )
+}
+
+export default Banner
