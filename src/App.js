c72e4a3c5094ab37ebc3cfd1f6872eb311f357d8
khamidovDev1 <khamidovsamandar5@gmail.com>
2023-03-30 14:13:06 +0500

    nav_bottom

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import './App.scss';
 import Navbar from './components/navbar/Navbar';
 import Footer from "./components/footer/Footer"
 import Products from './components/Products/Products';
+import NavBottom from './components/nav_bottom/NavBottom';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Products />
-      <Footer />
+      <NavBottom />
+      {/* <Products />
+      <Footer /> */}
 
     </div>
   );
